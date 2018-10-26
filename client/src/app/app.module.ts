e01f116b6b9350096b83935aca735e5b64fd317d
MacBookPro <macbookpro@McBookPro.local>
2018-10-26 17:17:53 -0500

    Create style when we select a client

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { ClientComponent } from './client/client.component';
 import { RegisterClientComponent } from './register-client/register-client.component';
+import { ClientDetailComponent } from './client-detail/client-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ClientComponent,
-    RegisterClientComponent
+    RegisterClientComponent,
+    ClientDetailComponent
   ],
   imports: [
     BrowserModule,
